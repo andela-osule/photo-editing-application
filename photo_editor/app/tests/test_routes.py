a0f3d870e24a15bc03a71148f0837330348b050f
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-30 22:10:04 +0100

    (chore) test sitemap, humans and robots are accessible

---
+++ photo_editor/app/tests/test_routes.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+
+class AppTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+
+    def test_can_get_sitemap_in_app_root(self):
+        """Test that sitemap.xml can be accessed"""
+        response = self.client.get('/sitemap.xml')
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_get_robots_in_app_root(self):
+        """Test that robots.txt file can be accessed"""
+        response = self.client.get('/robots.txt')
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_get_humans_in_app_root(self):
+        """Test that humans.txt file can be accessed"""
+        response = self.client.get('/humans.txt')
+        self.assertEqual(response.status_code, 200)
+
+    
