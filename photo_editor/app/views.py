9466181003ddc85b05becc2a3b613d2658c5be9b
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-30 22:13:16 +0100

    (feature) handle requests for root-based files

--- photo_editor/app/views.py
+++ photo_editor/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class RootFilesView(View):
+    """Renders requests for `sitemap.xml`, `robots.txt` and `humans.txt`"""
+
+    def get(self, request, *args, **kwargs):
+        """Returns response for `GET` request to the website root"""
+        filename = self.kwargs.get('filename')
+        return render(
+            request, filename,
+            {'page_title': filename}, content_type="text/plain"
+        )
+
+    
