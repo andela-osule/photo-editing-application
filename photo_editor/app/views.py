6c8ff2029cc2a7858d0081d47ccc1035c9fe1138
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-01 13:35:47 +0100

    (feature) update login view; add index view

--- photo_editor/app/views.py
+++ photo_editor/app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response
+from django.core.urlresolvers import reverse
 from django.views.generic import View
-
+from settings.base import FB_APP_ID, FB_SCOPE, FB_APP_SECRET
+from facebook import get_user_from_cookie, GraphAPI
 
 class RootFilesView(View):
     """Renders requests for `sitemap.xml`, `robots.txt` and `humans.txt`"""
@@ -13,4 +15,28 @@ class RootFilesView(View):
             {'page_title': filename}, content_type="text/plain"
         )
 
+
+class AuthView(View):
+    """Handles User authentication"""
     
+    def get(self, request):
+        """Handles `GET` request to the login route"""
+        
+        # Ensure that user has been authenticated before granting access to
+        # login
+        if request.user.is_authenticated():
+            return redirect(reverse('app.index'))
+        
+        context = {
+            'app_id': FB_APP_ID,
+            'allow_perms': ','.join(FB_SCOPE),
+        }
+
+        return render(request, 'app/login.html', context)
+    
+class AppView(View):
+    """Parley all requests that boister app functions
+    """
+
+    def get(self, request):
+        return render(request, 'app/index.html')
