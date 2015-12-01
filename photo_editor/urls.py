4c435d28b3513e174e231a3a5a9194e2cebc3bef
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-01 13:25:11 +0100

    (feature) add url mapping for static files

--- photo_editor/urls.py
+++ photo_editor/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from app import urls as app_urls
 
@@ -7,3 +9,5 @@ urlpatterns = [
     url(r'^', include(app_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
