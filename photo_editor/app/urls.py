8bfca387fd00f66d0998acd71ecfa78de9628ca6
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-01 14:40:27 +0100

    (fix) update URL for app root

--- photo_editor/app/urls.py
+++ photo_editor/app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 import app.views as views
 
 urlpatterns = [
-        url(r'^$', views.AppView.as_view(), name='app.index'),
+        url(r'^/$', views.AppView.as_view(), name='app.index'),
         url(r'^login/$', views.AuthView.as_view(), name='app.auth.login'),
         url(r'^(?P<filename>(sitemap.xml)|(robots.txt)|(humans.txt))$',
             views.RootFilesView.as_view(), name='app.root_files'),
