5496fc543269b6d9f9e4175961f98a1ae3fbd23e
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-01 13:26:14 +0100

    (feature) add SocialUser model

--- photo_editor/app/models.py
+++ photo_editor/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.http import request
 
-# Create your models here.
+class SocialUser(AbstractBaseUser):
+    name = models.CharField(null=False, max_length=256, default='')
+    profile_url = models.TextField(null=True)
+    access_token = models.TextField(null=True)
+    id = models.BigIntegerField(unique=True, primary_key=True)
+    USERNAME_FIELD = 'id'
+    
+    def has_valid_access_token(self, token):
+        """Affirms that the user has a valid access token
+        """
+        return self.access_token == token
+
+    def is_authenticated(self):
+        """Affirms that the user is authenticated"""
+        return self.active
