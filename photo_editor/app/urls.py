6f8360f8e3bb7a4e55646ec7c903e1091e442079
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-01 13:28:37 +0100

    (feature) add urls for index and login page

--- photo_editor/app/urls.py
+++ photo_editor/app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 import app.views as views
 
 urlpatterns = [
+        url(r'^$', views.AppView.as_view(), name='app.index'),
+        url(r'^login/$', views.AuthView.as_view(), name='app.auth.login'),
         url(r'^(?P<filename>(sitemap.xml)|(robots.txt)|(humans.txt))$',
             views.RootFilesView.as_view(), name='app.root_files'),
     ]
