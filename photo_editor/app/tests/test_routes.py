cee847c2b2df872ea3234c1017e30b1baf7f278a
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-08 18:23:20 +0100

    (feature) test that user can logout

--- photo_editor/app/tests/test_routes.py
+++ photo_editor/app/tests/test_routes.py
@@ -1,33 +1,43 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 
+
 class AppTestCase(TestCase):
-    
+
     def setUp(self):
         self.client = Client()
 
     def test_can_get_sitemap_in_app_root(self):
-        """Test that sitemap.xml can be accessed"""
+        '''Test that sitemap.xml can be accessed'''
         response = self.client.get('/sitemap.xml')
         self.assertEqual(response.status_code, 200)
 
     def test_can_get_robots_in_app_root(self):
-        """Test that robots.txt file can be accessed"""
+        '''Test that robots.txt file can be accessed'''
         response = self.client.get('/robots.txt')
         self.assertEqual(response.status_code, 200)
 
     def test_can_get_humans_in_app_root(self):
-        """Test that humans.txt file can be accessed"""
+        '''Test that humans.txt file can be accessed'''
         response = self.client.get('/humans.txt')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_guest_can_access_login_page(self):
-        """Test that guest can access login page"""
+        '''Test that guest can access login page'''
         response = self.client.get(reverse('app.auth.login'))
         self.assertEqual(response.status_code, 200)
 
+    def test_auth_user_can_logout(self):
+        '''Test that authenticated user can logout'''
+        response = self.client.get(reverse('app.auth.logout'))
+        self.assertRedirects(
+            response,
+            reverse('app.auth.login'),
+            status_code=302
+        )
+
     def test_guest_cannot_access_index_page(self):
-        """Test that guest can not have unauthorised access to app root
-        """
+        '''Test that guest can not have unauthorised access to app root
+        '''
         response = self.client.get(reverse('app.index'))
         self.assertEqual(response.status_code, 302)
