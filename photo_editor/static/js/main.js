87f5714f640078393101dc541aa94ad38daf2c40
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-08 19:10:58 +0100

    (feature) handle photo upload when upload button clicked

--- photo_editor/static/js/main.js
+++ photo_editor/static/js/main.js
@@ -1,27 +1,71 @@
 $(document).ready(function(){
-	$('.login-wrapper').on('click', fb_login);
+    /*
+    |-------------------------------------------------
+    | Facebook Authentication
+    |-------------------------------------------------
+    */
+	$('.login-wrapper').on('click', fbLogin);
+    /*
+    |-------------------------------------------------
+    | Show File Selection Window
+    |-------------------------------------------------
+    |
+    */
+    $('#uploadBtn').on('click', showUploadWnd);
+    /*
+    |-------------------------------------------------
+    | Verify file selected for upload
+    |-------------------------------------------------
+    */
+    $('input[name=image]').bind('change', verifyFileSelected);
+
 });
 
-function fb_login(){
-    FB.login(function(response) {
-
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            //console.log(response); // dump complete info
-            access_token = response.authResponse.accessToken; //get access token
-            user_id = response.authResponse.userID; //get FB UID
-
-            FB.api('/me', function(response) {
-                user_email = response.email; //get user email
-          // you can store this data into your database             
-            });
-
+var fbLogin = function (){
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            window.location = '/';
         } else {
-            //user hit cancel button
-            console.log('User cancelled login or did not fully authorize.');
-
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    console.log('Welcome!  Fetching your information.... ');
+                    //get access token
+                    access_token = response.authResponse.accessToken;
+                    //get FB UID
+                    user_id = response.authResponse.userID;
+                    FB.api('/me', function(response) {
+                        //get user email
+                        user_email = response.email;
+	                   // you can store this data into your database
+                    });
+                } else {
+                //user hit cancel button
+                    console.log('User cancelled login or did not fully authorize.');
+                }
+            }, {
+	           scope: $('.facebook').attr('data-scope')
+            });
         }
-    }, {
-        scope: $('.facebook').attr('data-scope')
     });
+}
+
+var showUploadWnd = function() {
+    $('input[name=image').click();
+}
+
+var verifyFileSelected = function() {
+    var fakeFilePath = $(this).val();
+    var pathSplit = fakeFilePath.split('.');
+    var fileExtension = pathSplit[pathSplit.length - 1]
+    var allowedFileTypes =  ['png', 'jpg'];
+    /*
+    |--------------------------------------------------
+    | Check if file extension is right then submit
+    |--------------------------------------------------
+    | form
+    |--------------------------------------------------
+    */
+    if ( allowedFileTypes.indexOf(fileExtension) !== -1 ){
+        $('form').submit();
+    }
 }
