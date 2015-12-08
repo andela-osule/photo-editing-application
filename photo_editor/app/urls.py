2cb2e586a2e761d5a7e636c7ae24d2862d80a950
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-08 18:13:37 +0100

    (feature) add `/photos` and `/logout` URLconfs

--- photo_editor/app/urls.py
+++ photo_editor/app/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 import app.views as views
 
 urlpatterns = [
-        url(r'^/$', views.AppView.as_view(), name='app.index'),
+        url(r'^$', views.AppView.as_view(), name='app.index'),
         url(r'^login/$', views.AuthView.as_view(), name='app.auth.login'),
+        url(r'^photos/$', views.JSONPhotosView.as_view(), name='app.photos'),
+        url(r'^logout/$', views.DeAuthView.as_view(), name='app.auth.logout'),
         url(r'^(?P<filename>(sitemap.xml)|(robots.txt)|(humans.txt))$',
             views.RootFilesView.as_view(), name='app.root_files'),
     ]
