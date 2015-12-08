03ae3d57d6796e8b8001f32feee013c238baaa61
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-08 18:00:11 +0100

    (fix) removed unused imports, make middleware lean

--- photo_editor/app/middleware.py
+++ photo_editor/app/middleware.py
@@ -1,45 +1,65 @@
-from django.shortcuts import render, redirect, render_to_response
-from django.core.urlresolvers import reverse
+from django.conf import settings
 from facebook import get_user_from_cookie, GraphAPI
-from settings.base import FB_APP_ID, FB_SCOPE, FB_APP_SECRET
 from .models import SocialUser as User
 
 
 class SocialBinderMiddleware():
-    
+
     def process_request(self, request):
-        if request.session.get('user'):
-            request.user = request.session.get('user')
-    
+        user = request.session.get('user', None)
+        # Bind user to request if user is in session
+        if user:
+            request.user = User.objects.get(id=user['id'])
+        return
+
     def process_response(self, request, response):
-        
-        result = get_user_from_cookie(
-                cookies=request.COOKIES,
-                app_id=FB_APP_ID,
-                app_secret=FB_APP_SECRET)
+        result = None
+        # Hoo! Active cookies, verify that user cookie isn't stale
+        if request.COOKIES:
+            try:
+                result = get_user_from_cookie(
+                    cookies=request.COOKIES,
+                    app_id=settings.FB_APP_ID,
+                    app_secret=settings.FB_APP_SECRET
+                )
+            except:
+                pass
 
         # If there is no result, we assume the user is not logged in.
         if result:
             # Check to see if this user is already in our database.
-            user = User.objects.filter(id = result.get('uid')).first()
-    
+            graph = GraphAPI(result['access_token'])
+            profile = dict()
+            user = User.objects.filter(id=result.get('uid')).first()
             if not user:
-                # Not an existing user so get info
-                graph = GraphAPI(result['access_token'])
-                profile = graph.get_object('me')
+                # User doesn't exist in datastore so get info
+                profile = graph.get_object('/me')
                 # Create the user and insert it into the database
-                user = User(id=str(profile.get('id')), name=profile.get('name'),
-                            profile_url=profile.get('link'),
-                            access_token=result.get('access_token'))
+                user = User(
+                    id=str(profile.get('id')),
+                    name=profile.get('name'),
+                    profile_url=profile.get('link'),
+                    access_token=result.get('access_token')
+                )
             elif not user.has_valid_access_token(result.get('access_token')):
                 # If an existing user, update the access token
                 user.access_token = result.get('access_token')
-    
+
+            graph_query_result = graph.get_object('/me/picture')
+            profile['link'] = graph_query_result['url']
+            if user.profile_url is not profile['link']:
+                user.profile_url = profile['link']
+
+            user.active = True
+
             # Add the user to the current session
-            response.session['user'] = dict(name=user.name,
-                                           profile_url=user.profile_url,
-                                           id=user.id,
-                                           access_token=user.access_token)
+            request.session['user'] = dict(
+                name=user.name,
+                profile_url=user.profile_url,
+                id=user.id,
+                access_token=user.access_token,
+                is_authenticated=user.is_authenticated()
+            )
 
             user.save()
         return response
