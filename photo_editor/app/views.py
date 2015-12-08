2870a564c997bbd6eeaccfc91e4529c01697c6c3
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-08 18:27:56 +0100

    (feature) reorder imports; using setting configuration from `django.conf`; add DeAuthView; add `post` method of AppView; add JSONPhotosView

--- photo_editor/app/views.py
+++ photo_editor/app/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render, redirect, render_to_response
-from django.core.urlresolvers import reverse
+from .models import Photo
+from .forms import PhotoForm
+from django.conf import settings
+from django.contrib import messages
+from django.http import JsonResponse
 from django.views.generic import View
-from settings.base import FB_APP_ID, FB_SCOPE, FB_APP_SECRET
-from facebook import get_user_from_cookie, GraphAPI
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+
 
 class RootFilesView(View):
-    """Renders requests for `sitemap.xml`, `robots.txt` and `humans.txt`"""
+    '''Renders requests for `sitemap.xml`, `robots.txt` and `humans.txt`'''
 
     def get(self, request, *args, **kwargs):
-        """Returns response for `GET` request to the website root"""
+        '''Returns response for `GET` request to the website root'''
         filename = self.kwargs.get('filename')
         return render(
             request, filename,
@@ -17,29 +21,75 @@ class RootFilesView(View):
 
 
 class AuthView(View):
-    """Handles User authentication"""
-    
+    '''Handles User authentication'''
+
     def get(self, request):
-        """Handles `GET` request to the login route"""
-        
+        '''Handles `GET` request to the login route'''
+
         # Ensure that user has been authenticated before granting access to
         # login
+
         if request.user.is_authenticated():
             return redirect(reverse('app.index'))
-        
+
         context = {
-            'app_id': FB_APP_ID,
-            'allow_perms': ','.join(FB_SCOPE),
+            'app_id': settings.FB_APP_ID,
+            'allow_perms': ','.join(settings.FB_SCOPE),
         }
 
         return render(request, 'app/login.html', context)
-    
+
+
+class DeAuthView(View):
+
+    '''De-authenticates user'''
+    def get(self, request):
+        '''Handle the de-authentication of a user'''
+        request.session.flush()
+        request.COOKIES = {
+            k: v for k, v in request.COOKIES.iteritems()
+            if not k.startswith('fbsr_')
+        }
+        return redirect(reverse('app.auth.login'))
+
+
 class AppView(View):
-    """Parley all requests that boister app functions
-    """
+    MSGS = {
+        'SUCCESS': 'Photo successfully submitted for upload',
+        'ERROR': 'Photo failed to upload at this time.'
+    }
+
+    '''Parley all requests that boister app functions'''
 
     def get(self, request):
+        context = {}
         if not request.user.is_authenticated():
             return redirect(reverse('app.auth.login'))
-        
-        return render(request, 'app/index.html')
+        else:
+            context['photos'] = Photo.objects.filter(user=request.user)
+        return render(request, 'app/index.html', context)
+
+    def post(self, request):
+        '''Upload files to Cloudinary'''
+        request.POST['user'] = request.user.id
+        request.POST['title'] = ''
+        photo_form = PhotoForm(
+            request.POST,
+            request.FILES,
+        )
+        if photo_form.is_valid():
+            messages.add_message(
+                request, messages.SUCCESS, AppView.MSGS['SUCCESS']
+            )
+            photo_form.save()
+        else:
+            messages.add_message(
+                request, messages.ERROR, AppView.MSGS['ERROR']
+            )
+        return redirect(reverse('app.index'))
+
+
+class JSONPhotosView(View):
+    def get(self, request):
+        serialized_photos = Photo.get_for_user(request.user)
+        return JsonResponse(serialized_photos, safe=False)
