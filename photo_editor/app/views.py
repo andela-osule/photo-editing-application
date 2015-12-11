304ba1d3de7a6455393d6c65a245d446f36d688a
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-11 16:27:39 +0100

    (feature) allow for deletion, upload and retrieval of photos

--- photo_editor/app/views.py
+++ photo_editor/app/views.py
@@ -1,11 +1,11 @@
 from .models import Photo
 from .forms import PhotoForm
 from django.conf import settings
-from django.contrib import messages
 from django.http import JsonResponse
 from django.views.generic import View
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 
 class RootFilesView(View):
@@ -55,8 +55,8 @@ class DeAuthView(View):
 
 class AppView(View):
     MSGS = {
+        'ERROR':   'Photo failed to upload at this time.',
         'SUCCESS': 'Photo successfully submitted for upload',
-        'ERROR': 'Photo failed to upload at this time.'
     }
 
     '''Parley all requests that boister app functions'''
@@ -71,25 +71,63 @@ class AppView(View):
 
     def post(self, request):
         '''Upload files to Cloudinary'''
+        pass
+
+
+class JSONPhotosView(View):
+    '''Returns photos belonging to authenticated user as JSON
+    '''
+    def get(self, request):
+        serialized_photos = {'photos': Photo.get_for_user(request.user)}
+        return JsonResponse(serialized_photos, safe=False)
+
+
+class DestroyPhotoView(View):
+    '''Deletes photo from the database'''
+    MSGS = {
+        'ERROR':   'Photo does not exist',
+        'SUCCESS': 'Photo was deleted successfully',
+    }
+
+    def get(self, request, photo_id):
+        mesgs = dict()
+        try:
+            photo = Photo.objects.get(id=photo_id, user=request.user)
+            mesgs['tags'] = 'success'
+            mesgs['text'] = DestroyPhotoView.MSGS['SUCCESS']
+            photo.delete()
+        except Photo.DoesNotExist:
+            mesgs['tags'] = 'danger'
+            mesgs['text'] = DestroyPhotoView.MSGS['ERROR']
+        return JsonResponse({'messages': mesgs})
+
+
+class UploadPhotoView(View):
+    '''Uploads photo to the database'''
+    MSGS = {
+        'ERROR':   'Photo failed to upload at this time.',
+        'SUCCESS': 'Photo successfully submitted for upload',
+    }
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(UploadPhotoView, self).dispatch(*args, **kwargs)
+
+    def post(self, request):
         request.POST['user'] = request.user.id
         request.POST['title'] = ''
+        mesgs = dict()
         photo_form = PhotoForm(
             request.POST,
             request.FILES,
         )
         if photo_form.is_valid():
-            messages.add_message(
-                request, messages.SUCCESS, AppView.MSGS['SUCCESS']
-            )
+            mesgs['tags'] = 'success'
+            mesgs['text'] = UploadPhotoView.MSGS['SUCCESS']
             photo_form.save()
+            uploaded_photo = Photo.get_for_user(request.user, latest=True)
         else:
-            messages.add_message(
-                request, messages.ERROR, AppView.MSGS['ERROR']
-            )
-        return redirect(reverse('app.index'))
-
-
-class JSONPhotosView(View):
-    def get(self, request):
-        serialized_photos = Photo.get_for_user(request.user)
-        return JsonResponse(serialized_photos, safe=False)
+            mesgs['tags'] = 'danger'
+            mesgs['text'] = UploadPhotoView.MSGS['ERROR']
+        response_data = {'messages': mesgs, 'photo': uploaded_photo.pop()}
+        return JsonResponse(response_data)
