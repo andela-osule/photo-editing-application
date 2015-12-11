50f86edc9caf1f788339e08bfcdcb21979ef33fa
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-11 16:26:31 +0100

    (feature) allow for deletion of user photo by id

--- photo_editor/app/urls.py
+++ photo_editor/app/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
         url(r'^login/$', views.AuthView.as_view(), name='app.auth.login'),
         url(r'^photos/$', views.JSONPhotosView.as_view(), name='app.photos'),
         url(r'^logout/$', views.DeAuthView.as_view(), name='app.auth.logout'),
+        url(r'^photo/(?P<photo_id>[0-9]+)/delete/$', views.DestroyPhotoView
+            .as_view(), name='app.photo.destroy'),
+        url(r'^upload/$', views.UploadPhotoView.as_view(),
+            name='app.photo.upload'),
         url(r'^(?P<filename>(sitemap.xml)|(robots.txt)|(humans.txt))$',
             views.RootFilesView.as_view(), name='app.root_files'),
     ]
