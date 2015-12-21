e60888c4ff12e1eb058a4cbd937e8aadcf5c8f87
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-21 20:05:19 +0100

    (feature) retire PhotoFxCtrl, add sharePhoto directive

--- photo_editor/static/js/app.js
+++ photo_editor/static/js/app.js
@@ -1,10 +1,16 @@
-angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
+angular.module('photoApp', ['angularMoment', 'slickCarousel', 'PhotoEffectsSvc', 'ngFileUpload', 'ngAnimate'])
     .config(function($interpolateProvider){
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
     })
+    .config(
+        ['slickCarouselConfig', function(slickCarouselConfig){
+            slickCarouselConfig.dots = false;
+            slickCarouselConfig.autoplay = false;
+        }]
+    )
     .controller(
-        'PhotosCtrl', function($scope, $http){
+        'PhotosCtrl', function($scope, $http, $rootScope, $timeout){
 /*
 |-------------------------------------------------------------
 | Set photos in controller scope
@@ -15,22 +21,30 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
                 $scope.photos = responseData.photos;
             }
         );
-        $scope.DestroyAlert = function() {
-            $('.alert').addClass('hide');
-        }
+        $rootScope.ShowSublimalAlert = function(){
+            $timeout(function(){
+                $scope.showAlert = true;
+            }, 700);
+            $timeout(function(){
+                $scope.showAlert = false;
+            }, 3500);
+        };
+        $rootScope.DestroyAlert = function() {
+              $scope.showAlert = false;
+        };
         $scope.UpdatePhotoTitle = function($event) {
-            if($event.which == 0 | $event.which == 13)
+            if($event.which === 0 | $event.which == 13)
             {
                 var photoId = this.photo.id;
-                var photoTitle = $($event.currentTarget);
+                var photoTitle = angular.element($event.currentTarget);
                 var scopeTitle = this.photo.title;
                 var _this = this;
                 if(photoTitle.val() != scopeTitle) {
                 $http.post('/photo/'+photoId+'/update/', {title: photoTitle.val()})
                 .then(function(response){
-                    console.log(response.data.messages);
                     _this.photo.edited_at = response.data.edited_at;
                     $scope.messages = [response.data.messages];
+                    $rootScope.ShowSublimalAlert();
                 }, function(response){
                     console.log('An error occurred');
                 });
@@ -39,9 +53,9 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
                 photoTitle.prev().removeClass('hidden');
                 photoTitle.addClass('hidden');
             }
-        }
+        };
     }).controller(
-        'PhotoCtrl', ['$scope', '$http', 'Upload', function($scope, $http, Upload){
+        'PhotoCtrl', ['$scope', '$http', '$rootScope', 'Upload', function($scope, $http, $rootScope, Upload){
             $scope.upload = function ($files) {
                 $scope.doUpload($files[0]);
             };
@@ -52,16 +66,15 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
                 }).then(function (response) {
                     $scope.$parent.messages = [response.data.messages];
                     $scope.$parent.photos.unshift(response.data.photo);
-                    $scope.$parent.DestroyAlert();
+                    $rootScope.ShowSublimalAlert();
                 }, function (response) {
                     console.log('Error status: ' + response.status);
                 }, function (event) {
                     var progressPercentage = parseInt(100.0 * event.loaded / event.total);
-                    console.log('progress: ' + progressPercentage + '% ');
+                    $scope.$parent.messages = ['progress: ' + progressPercentage + '%'];
                 });
             };
-        }
-    ])
+    }])
     .directive(
 /*
 |-------------------------------------------------------------
@@ -70,16 +83,17 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
 */
         'selectPhoto', ['$document', function($document) {
             return {
+                restrict: 'A',
                 templateUrl: 'static/Partials/selection.html',
                 link: function (scope, element, attr) {
                     element.on('click', function(event){
                         event.preventDefault();
-                        $('.selected').toggleClass('selected');
-                        $(this).toggleClass('selected');
-                        $('#photo').attr({'src':$(this).attr('data-filesrc')});
+                        angular.element('.selected').toggleClass('selected');
+                        angular.element(this).toggleClass('selected');
+                        angular.element('#photo').attr({'src':angular.element(this).attr('data-filesrc')});
                     });
                 }
-            }
+            };
     }])
     .directive(
 /*
@@ -87,27 +101,30 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
 | Set directive to delete a selected photo
 |-------------------------------------------------------------
 */
-        'deletePhoto', ['$document', '$http', function($document, $http) {
-            return function (scope, element, attr) {
+        'deletePhoto', ['$document', '$http', '$rootScope', function($document, $http, $rootScope) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attr) {
                     element.on('click', function(event){
                         event.preventDefault();
                         var selectedPhoto = $('.selected');
                         var photoId = selectedPhoto.attr('data-id');
-                        if(photoId == undefined){
+                        if(photoId === undefined){
                             scope.messages = [{'tags':'info', 'text':'Select a photo to delete'}];
-                            scope.$apply();
                         }
                         else {
                             selectedPhoto.remove();
                             $http.get('/photo/'+photoId+'/delete').success(
                                 function(responseData){
                                     scope.messages = responseData;
-                                    scope.DestroyAlert();
+                                    $rootScope.ShowSublimalAlert();
                                 }
                             );
+                            angular.element('#photo').removeAttr('src').hide();
                         }
                     });
                 }
+            };
         }])
     .directive(
 /*
@@ -115,31 +132,87 @@ angular.module('photoApp', ['angularMoment', 'ngFileUpload', 'ngAnimate'])
 | Set directive to edit a selected photo
 |-------------------------------------------------------------
 */
-        'editPhoto', ['$document', '$http', function ($document, $http) {
-            return function (scope, element, attr) {
-                element.on('click', function(event){
-                    event.preventDefault();
-                    var selectedPhoto = $('.selected');
-                    var photoId = selectedPhoto.attr('data-id');
-                    if(photoId == undefined){
-                        scope.messages = [{'tags':'info', 'text':'Select a photo to edit'}];
-                        scope.$apply();
-                    }
-                    else {
-                    }
-                });
-            }
+        'editPhoto', ['$http', 'EffectsDS', function ($http, $EffectsDS) {
+            return {
+                restrict: 'A',
+                link:function (scope, element, attr) {
+                    element.on('click', function(event){
+                        event.preventDefault();
+                        var selectedPhoto = angular.element('.selected');
+                        var photoId = selectedPhoto.attr('data-id');
+                        if(photoId === undefined){
+                            scope.$apply(function(){
+                                scope.messages = [{'tags':'info', 'text':'Select a photo to edit'}];
+                                scope.ShowSublimalAlert();
+                            });
+                        }
+                        else {
+                            scope.$apply(function(){
+                                //scope.fxCollection = $EffectsDS.getEffects();
+                                $EffectsDS.getEffects().then(function(data)
+                                    {
+                                        scope.effectList  = data;
+                                        scope.effectIsLoaded = $EffectsDS.getStatus();
+                                    });
+                            });
+                        }
+                    });
+                }
+            };
         }])
     .directive(
-        'editPhotoTitle', ['$document', '$http', '$compile', function ($document, $compile, $http) {
-            return function (scope, element, attr) {
-                element.on('click', function (event, $http){
-                        var input = $(this).next();
-                        input.toggleClass('hidden').focus();
-                        var oldValue = input.val();
-                        input.val('');
-                        input.val(oldValue);
-                        $(this).toggleClass('hidden');
-                });
-            }
+        'editPhotoTitle', ['$http', function ($http) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attr) {
+                    element.on('click', function (event, $http){
+                            var input = angular.element(this).next();
+                            input.toggleClass('hidden').focus();
+                            var oldValue = input.val();
+                            input.val('');
+                            input.val(oldValue);
+                            angular.element(this).toggleClass('hidden');
+                    });
+                }
+            };
+        }])
+    .directive(
+        'fxApply', ['$http', function ($http){
+            return {
+                restrict: 'A',
+                link: function(scope, element, attr) {
+                    element.on('click', function (){
+                        var photoId = angular.element('.selected').attr('data-id');
+                        $http.get('/photo/'+ photoId + '/fx/' + scope.effect.name + '/').success(
+                            function(response){
+                                angular.element('#photo').attr({'src':response.fxSrc}).show();
+                            }
+                        );
+                    });
+                }
+            };
+        }])
+    .directive(
+        'sharePhoto', ['$http', function($http){
+            return {
+                restrict: 'A',
+                link: function(scope, element, attr) {
+                    element.on('click', function() {
+                        var transformPhotoSrc = angular.element('#photo').attr('src');
+                        if (transformPhotoSrc !== undefined){
+                            $http.post('/photo/share/', {src:transformPhotoSrc})
+                            .success(
+                                function(response){
+                                    scope.messages = [{'tags':'info', 'text':'Photo URI has been shared to your timeline.'}];
+                                    scope.ShowSublimalAlert();
+                                }
+                            ).error(function(response){
+                                scope.messages = [{'tags':'danger', 'text':'An error occurred.'}];
+                                scope.ShowSublimalAlert();
+                            });
+                        }
+                    });
+                }
+            };
+
         }]);
