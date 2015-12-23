7231d729ae48457566a0b91e17f87f3d088d08ca
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-12-23 08:15:44 +0100

    (chore) update grayscale filter

--- photo_editor/app/proxy.py
+++ photo_editor/app/proxy.py
@@ -1,7 +1,9 @@
 from PIL import ImageFilter
+from itertools import chain
 
 
 class ALIEN(ImageFilter.MedianFilter):
+    '''Alien filter'''
     name = "Alien"
 
     def __init__(self):
@@ -10,6 +12,7 @@ class ALIEN(ImageFilter.MedianFilter):
 
 
 class BLURMORE(ImageFilter.GaussianBlur):
+    '''Blur more filter'''
     name = "Blur more"
 
     def __init__(self):
@@ -17,6 +20,7 @@ class BLURMORE(ImageFilter.GaussianBlur):
 
 
 class BRIGHTEN(ImageFilter.BuiltinFilter):
+    '''Brighten filter'''
     name = "Brighten"
     filterargs = (3, 3), 16, 0, (
         -2, -24, -2,
@@ -26,6 +30,7 @@ class BRIGHTEN(ImageFilter.BuiltinFilter):
 
 
 class SHARPENMORE(ImageFilter.BuiltinFilter):
+    '''Sharpen filter'''
     name = "Sharpen more"
     filterargs = (3, 3), 11, 0, (
         -2, -2, -2,
@@ -34,28 +39,27 @@ class SHARPENMORE(ImageFilter.BuiltinFilter):
     )
 
 
-def make_linear_ramp(white):
-    # putpalette expects [r,g,b,r,g,b,...]
+def make_linear_ramp(color):
+    '''Generate linear ramp'''
     ramp = []
-    r, g, b = white
+    r, g, b = color
     for i in xrange(255):
         ramp.extend((r * i / 255, g * i / 255, b * i / 255))
     return ramp
 
 
-def make_grayscale(color):
-    ramp = []
-    r, g, b = color
-    for i in xrange(255):
-        ramp.extend(((0.2989 * r), (0.5870 * g), (0.1140 * b)))
+def make_grayscale():
+    '''Generate grayscale'''
+    data = range(256)
+    ramp = list(map(lambda v: (v, v, v), data))
+    ramp = list(chain.from_iterable(ramp))
     return ramp
-
 
 SEPIA = make_linear_ramp((255, 240, 192))
 
 WHOOPS = make_linear_ramp((250, 100, 23))
 
-GRAYSCALE = make_grayscale((100, 100, 100))
+GRAYSCALE = make_grayscale()
 
 CLASS_MAP = {
     'ALIEN': ALIEN,
